c1682015f49dd96a56bb125c09e74cba29290da0
Florian <jfanor@bbdo.fr>
2017-12-13 13:40:38 +0400

    First commit

---
+++ js/contact_form.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('#form').parsley();
+    $("#submit").click(function() {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
+        var contact = $("#contact").val();
+        $("#returnmessage").empty(); // To empty previous error/success message.
+
+        // Checking for blank fields.
+        if (name == '' || email == '' || contact == '') {
+            alert("Please Fill Required Fields");
+        } else
+            alert("It is working");
+
+    });
+});
