a02a64f75587e4ddeb07b4b8199f4e0a5bf110cb
Florian <jfanor@bbdo.fr>
2017-12-13 15:15:13 +0400

    Change on button

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,25 +1,5 @@
-$(function() {
-
-    $("#contact").mask("(999) 999-9999");
-
-
-    $("#contact").on("blur", function() {
-        var last = $(this).val().substr($(this).val().indexOf("-") + 1);
-
-        if (last.length == 5) {
-            var move = $(this).val().substr($(this).val().indexOf("-") + 1, 1);
-
-            var lastfour = last.substr(1, 4);
-
-            var first = $(this).val().substr(0, 9);
-
-            $(this).val(first + move + '-' + lastfour);
-        }
-    });
-});
-
 $(document).ready(function() {
-    $('#form').parsley();
+    $("#form").parsley();
     $("#submit").click(function() {
         var name = $("#name").val();
         var email = $("#email").val();
