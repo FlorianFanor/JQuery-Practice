b288c68aa42494e5bd644af3a9abcc5a876943b9
Florian <jfanor@bbdo.fr>
2017-12-14 08:46:31 +0400

    Update bootstrap

--- js/contact_form.js
+++ js/contact_form.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
+    $("button").click(function() {
+        $("form").toggle();
+        $("#form").reset();
+    });
+
     $("#form").parsley();
-    $("#submit").click(function() {
+
+    $("#submit").click(function(e) {
+        e.preventDefault();
         var name = $("#name").val();
         var email = $("#email").val();
         var message = $("#message").val();
@@ -8,31 +15,37 @@ $(document).ready(function() {
         $("#returnmessage").empty(); // To empty previous error/success message.
 
         // Checking for blank fields.
-        if (name == '' || email == '' || contact == '') {
-            alert("Please Fill Required Fields");
-        } else {
-            $.ajax({
-                    method: "POST",
-                    url: "https://wt-4e8c7e4544b5cacf32167cf90e12dfeb-0.run.webtask.io/contactform/contact",
-                    data: {
-                        "name": name,
-                        "email": email,
-                        "message": message,
-                        "contact": contact
-                    }
-                })
-                .done(function(msg) {
-                    alert("Data Saved: " + msg);
-                    $("#returnmessage").append(msg);
-                    $("#form")[0].reset();
-                })
-                .fail(function(msg) {
-                    alert("failed");
-                })
-                .always(function(msg) {
-                    //alert( "complete" );
-                });
+        $.ajax({
+                method: "POST",
+                url: "https://wt-4e8c7e4544b5cacf32167cf90e12dfeb-0.run.webtask.io/contactform/contact",
+                data: {
+                    "name": name,
+                    "email": email,
+                    "message": message,
+                    "contact": contact
+                }
+            })
+            .done(function(data) {
+                //alert("Data Saved: " + msg);
+                //data = JSON.stringify(data);
+                console.log(data.type);
+                console.log(data.message);
+                var msg = "<div class='" + data.type + "'>" + data.message + "</div>";
+                $("#returnmessage").append(msg);
 
-        }
+
+                if (data.type === "success") {
+                    $("#form")[0].reset();
+                }
+
+            })
+            .fail(function(msg) {
+                //alert("failed");
+                var msg = "<div class='" + data.type + "'>" + data.message + "</div>";
+                $("#returnmessage").append(msg);
+            })
+            .always(function(msg) {
+                //alert( "complete" );
+            });
     });
 });
